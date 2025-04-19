901a6c2fcaadb185e659f07573103e489e6461a0
shuvo <shuvo6904@gmail.com>
2025-04-19 17:09:35 +0600

    # booklist screen in progress

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -1,9 +1,17 @@
 package com.plcoding.bookpedia
 
 import androidx.compose.runtime.*
+import com.plcoding.bookpedia.book.presentation.book_list.BookListScreenRoot
+import com.plcoding.bookpedia.book.presentation.book_list.BookListViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
+    BookListScreenRoot (
+        viewModel = remember { BookListViewModel() },
+        onBookClick = {
+
+        }
+    )
 }
