0afe29c0934ceccabf93aa18c5baf0211ae0e96c
shuvo <shuvo6904@gmail.com>
2025-04-21 10:35:20 +0600

    # in progress

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/data/HttpClientExt.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/data/HttpClientExt.kt
@@ -4,7 +4,27 @@ import com.plcoding.bookpedia.core.domain.DataError
 import com.plcoding.bookpedia.core.domain.Result
 import io.ktor.client.call.NoTransformationFoundException
 import io.ktor.client.call.body
+import io.ktor.client.network.sockets.SocketTimeoutException
 import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.ensureActive
+import kotlin.coroutines.coroutineContext
+
+suspend inline fun <reified T> safeCall(
+    execute: () -> HttpResponse
+): Result<T, DataError.Remote> {
+    val response = try {
+        execute()
+    } catch (e: SocketTimeoutException) {
+        return Result.Error(DataError.Remote.REQUEST_TIMEOUT)
+    } catch (e: UnresolvedAddressException) {
+        return Result.Error(DataError.Remote.NO_INTERNET)
+    } catch (e: Exception) {
+        coroutineContext.ensureActive()
+        return Result.Error(DataError.Remote.UNKNOWN)
+    }
+    return responseToResult(response)
+}
 
 suspend inline fun <reified T> responseToResult(
     response: HttpResponse
