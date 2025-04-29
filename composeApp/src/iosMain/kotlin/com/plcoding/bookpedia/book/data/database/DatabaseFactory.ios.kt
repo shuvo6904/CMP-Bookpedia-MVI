703186d8df070fe39e5c354f072356fbce9e0162
shuvo <shuvo6904@gmail.com>
2025-04-29 17:36:08 +0600

    # add room-database in-progress

---
+++ composeApp/src/iosMain/kotlin/com/plcoding/bookpedia/book/data/database/DatabaseFactory.ios.kt
@@ -0,0 +1,9 @@
+package com.plcoding.bookpedia.book.data.database
+
+import androidx.room.RoomDatabase
+
+actual class DatabaseFactory {
+    actual fun create(): RoomDatabase.Builder<FavoriteBookDatabase> {
+        TODO("Not yet implemented")
+    }
+}
